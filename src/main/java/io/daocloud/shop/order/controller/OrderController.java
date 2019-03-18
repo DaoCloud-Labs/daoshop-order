08141eb181abd4aa49f088dfda13313ffe42ed82
hao <dao.cheng@daocloud.io>
2019-03-18 11:25:37 +0800

    init

---
+++ src/main/java/io/daocloud/shop/order/controller/OrderController.java
@@ -0,0 +1,16 @@
+package io.daocloud.shop.order.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Package io.daocloud.shop.order.controller
+ * @Classname OrderController
+ * @Description TODO
+ * @Date 2019/3/17 下午10:45
+ * @Created by chenghao
+ * @desc OrderController
+ * @project order
+ */
+@RestController
+public class OrderController {
+}
---
+++ src/main/java/io/daocloud/shop/order/vo/OrderVo.java
@@ -0,0 +1,26 @@
+package io.daocloud.shop.order.vo;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Package io.daocloud.shop.order.vo
+ * @Classname OrderVo
+ * @Description TODO
+ * @Date 2019/3/17 下午10:48
+ * @Created by chenghao
+ * @desc OrderVo
+ * @project order
+ */
+
+@Data
+@NoArgsConstructor
+public class OrderVo {
+
+    private int count;
+    private double price;
+    private double amount;
+    private long productId;
+    private String productName;
+
+}
