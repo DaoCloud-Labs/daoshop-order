52aaf395079b5e59da643c20e564d95310d1a8f5
hao <dao.cheng@daocloud.io>
2019-03-19 14:51:22 +0800

    init

--- src/main/java/io/daocloud/shop/order/entity/OrderEntity.java
+++ src/main/java/io/daocloud/shop/order/entity/OrderEntity.java
@@ -1,5 +1,6 @@
 package io.daocloud.shop.order.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,6 +21,7 @@ import javax.persistence.*;
 @Table(name = "t_order")
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class OrderEntity {
     @Id
