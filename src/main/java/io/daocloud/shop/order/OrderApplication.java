6cea9e9adf676b3f6953bad978dfdcb9eca0c764
zhiyi yan <zhiyi.yan@daocloud.io>
2019-09-26 16:26:39 +0800

    add daocloud discovery

--- src/main/java/io/daocloud/shop/order/OrderApplication.java
+++ src/main/java/io/daocloud/shop/order/OrderApplication.java
@@ -1,9 +1,11 @@
 package io.daocloud.shop.order;
 
+import io.daocloud.annotation.EnableDaocloudDiscovery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDaocloudDiscovery
 public class OrderApplication {
 
     public static void main(String[] args) {
